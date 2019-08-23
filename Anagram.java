c06c53ba1a6cb3e92ee3a5f47e99bde85fdc68b7
Harshvardhan Singh <51012630+harshvar22dec@users.noreply.github.com>
2019-08-23 22:53:16 +0530

    Add files via upload

---
+++ Anagram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class Anagram{  
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+String s=sc.next();
+String t=sc.next();
+int c=0;
+byte i=0;
+for(;i<s.length();i++)
+{	for(byte j=0;j<t.length();j++)
+	{
+		if(s.charAt(i)==t.charAt(j)){c++;break;}
+	}
+}
+if(c==s.length()){System.out.print("YES");}
+else{System.out.print("NO");}
+}}

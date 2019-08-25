92f4519828ba80591839b90c008282a53d41e686
Harshvardhan Singh <51012630+harshvar22dec@users.noreply.github.com>
2019-08-25 22:26:59 +0530

    Add files via upload

---
+++ EvenOddJava.java
@@ -0,0 +1,6 @@
+import java.util.Scanner;
+class EvenOddJava{
+public static void main(String[] args)
+{Scanner sc=new Scanner(System.in);
+	System.out.print(((sc.nextInt()&1)==0)?"even":"odd");	
+}}

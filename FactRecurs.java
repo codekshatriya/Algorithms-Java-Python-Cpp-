77d7e250561bd151edb25d673dca92aa8e421a47
Harshvardhan Singh <51012630+harshvar22dec@users.noreply.github.com>
2019-12-29 21:03:36 +0530

    Add files via upload

---
+++ FactRecurs.java
@@ -0,0 +1,16 @@
+package p1;
+//Recursive Factorial Calculator, developed by Harshvardhan Singh(harshvardhan.singh22dec@gmail.com)
+//Created on Intellij idea,25 December 2019
+import java.util.Scanner;
+public class FactRecurs{
+    static int fac(int n)
+    {
+        return (n==0)?1:n*fac(--n);
+    }
+    static Scanner sc=new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        System.out.print("Enter Number to Get Factorial: ");
+        System.out.println(fac(sc.nextInt()));
+    }
+}

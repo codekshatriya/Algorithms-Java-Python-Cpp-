9150268bc601035ab1659f7a0eca409158044e77
Harshvardhan Singh <51012630+harshvarons@users.noreply.github.com>
2020-04-14 11:59:41 +0530

    Add files via upload
    Java Program to generate a random number

---
+++ RandomNoGenerJava.java
@@ -0,0 +1,11 @@
+/*
+Program to generate a random number(6 digit long)
+Created On,at & By: intelliJ Idea Pro,13 April 2020 & Harshvardhan Singh
+*/
+package Pack1;
+import java.time.LocalDateTime;
+public class RandomNoGenerJava{
+    public static void main(String[] args) {
+        System.out.println(LocalDateTime.now().toString().substring(20));
+    }
+}
